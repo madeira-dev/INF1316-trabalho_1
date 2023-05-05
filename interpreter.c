d948335bc6776545bbab06205db30172225f534f
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2023-05-05 17:01:36 -0300

    :construction: shared memory between interpreter and scheduler in development

--- interpreter.c
+++ interpreter.c
@@ -1,18 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <string.h>
+
+#define MAX_PROGRAM_NAME_LEN 20
+#define SHM_SIZE 1024
 
 int main(void)
 {
+    // variaveis file I/O
     FILE *file_ptr;
-    char program_name[20];
-    int start_time;    /* escalonamento round robin */
-    int duration_time; /* escalonamento round robin */
+    char program_name[MAX_PROGRAM_NAME_LEN];
+    int start_time;    /* escalonamento robinho */
+    int duration_time; /* escalonamento robinho */
     int c;             /* pra iterar pelos char do arquivo */
+
+    // variaveis memoria compartilhada
+    char *shm_program_name;
+    int segment, *shm_start_time, *shm_duration_time;
+
+    // criando segmento de memoria compartilhada
+    segment = shmget(14000, SHM_SIZE, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (segment == -1)
+    {
+        printf("erro no shmget\n");
+        exit(1);
+    }
+
+    // dando attach das variaveis na memoria compartilhada
+    shm_program_name = (char *)shmat(segment, NULL, 0);
+    if (shm_program_name == (void *)-1)
+    {
+        printf("erro no shmat\n");
+        exit(1);
+    }
+    shm_start_time = (int *)shm_program_name + 1; /* posicao seguinte de program_name */
+    if (shm_start_time == (void *)-1)
+    {
+        printf("erro no shmat\n");
+        exit(1);
+    }
+    shm_duration_time = (int *)shm_start_time + 1; /* posicao seguinte de start time*/
+    if (shm_duration_time == (void *)-1)
+    {
+        printf("erro no shmat\n");
+        exit(1);
+    }
 
     // abrindo em read mode
     file_ptr = fopen("exec.txt", "r");
 
+    // lendo linhas do arquivo
     while ((c = fgetc(file_ptr)) != EOF)
     {
         if (c == 'R')
@@ -23,11 +65,19 @@ int main(void)
                 {
                     printf("escalonameno robinho\n");
                     printf("%s %d %d\n", program_name, start_time, duration_time);
+
+                    // passando valores encontrados no arquivo para variaveis da memoria comparilhada
+                    strcpy(shm_program_name, program_name);
+                    *shm_start_time = start_time;
+                    *shm_duration_time = duration_time;
                 }
                 else
                 {
                     printf("escalonamento real time\n");
                     printf("%s\n", program_name);
+
+                    // passando nome encontrado no arquivo pra variavel na memoria compartilhada
+                    strcpy(shm_program_name, program_name);
                 }
             }
             else
@@ -36,9 +86,32 @@ int main(void)
                 exit(1);
             }
         }
-        sleep(2);
+        // sleep(2);
     }
     fclose(file_ptr);
+
+    // dettach de variaveis da memoria compartilhada
+    if (shmdt(shm_program_name) == -1)
+    {
+        printf("erro no shmdt program name\n");
+        exit(3);
+    }
+    if (shmdt(shm_start_time) == -1)
+    {
+        printf("erro no shmdt start time\n");
+        exit(3);
+    }
+    if (shmdt(shm_duration_time) == -1)
+    {
+        printf("erro no shmdt duration time\n");
+        exit(3);
+    }
+
+    if (shmctl(segment, IPC_RMID, 0) == -1)
+    {
+        printf("erro no shmctl\n");
+        exit(4);
+    }
 
     return 0;
 }
--- scheduler.c
+++ scheduler.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <string.h>
+
+#define MAX_PROGRAM_NAME_LEN 20
+#define SHM_SIZE 1024
 
 int main(void)
 {
+    int segment, *shm_start_time, *shm_duration_time;
+    char *shm_program_name;
+
+    segment = shmget(12000, SHM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (segment == -1)
+    {
+        printf("erro no shmget\n");
+        exit(1);
+    }
+
+    // attach de variaveis
+    shm_program_name = (char *)shmat(segment, 0, 0);
+    if (shm_program_name == (void *)-1)
+    {
+        printf("erro no shmat\n");
+        exit(1);
+    }
+    *shm_start_time = (int *)shmat(segment, 0, 0);
+    if (*shm_start_time == (void *)-1)
+    {
+        printf("erro no shmat\n");
+        exit(1);
+    }
+    *shm_duration_time = (int *)shmat(segment, 0, 0);
+    if (*shm_duration_time == (void *)-1)
+    {
+        printf("erro no shmat\n");
+        exit(1);
+    }
 
     return 0;
 }
