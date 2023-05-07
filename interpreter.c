b5331dd4c0a407b47e7cf53964ece13d747260eb
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2023-05-07 16:03:27 -0300

    :sparkles: communication between interpreter and scheduler developed

--- interpreter.c
+++ interpreter.c
@@ -5,8 +5,8 @@
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <signal.h>
 #include <string.h>
-#include <errno.h>
 
 #define MAX_PROGRAM_NAME_LEN 20
 #define SHM_SIZE 1024
@@ -15,17 +15,17 @@ int main(void)
 {
     // variaveis file I/O
     FILE *file_ptr;
-    char program_name[MAX_PROGRAM_NAME_LEN];
-    int start_time;    /* escalonamento robinho */
-    int duration_time; /* escalonamento robinho */
-    int c;             /* pra iterar pelos char do arquivo */
+    char program_name[MAX_PROGRAM_NAME_LEN], exec_mode[] = "1";
+    int start_time, duration_time; /* escalonamento robinho */
+    int c, scheduler_pid, pid, status;
+    char *args[] = {"scheduler", exec_mode, NULL};
 
     // variaveis memoria compartilhada
     char *shm_program_name;
     int segment, *shm_start_time, *shm_duration_time;
 
     // criando segmento de memoria compartilhada
-    segment = shmget(48000, SHM_SIZE, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    segment = shmget(2000, SHM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
     if (segment == -1)
     {
         printf("erro no shmget\n");
@@ -51,56 +51,89 @@ int main(void)
         exit(1);
     }
 
-    // abrindo em read mode
-    file_ptr = fopen("exec.txt", "r");
-
-    // lendo linhas do arquivo
-    while ((c = fgetc(file_ptr)) != EOF)
+    // criando processo filho para poder executar o scheduler e descobrir o pid dele
+    pid = fork();
+    if (pid == -1)
     {
-        if (c == 'R')
+        printf("erro no fork()\n");
+        exit(1);
+    }
+    if (pid == 0)
+    {
+        printf("entrei no processo filho\n");
+        // processo filho
+        execv("scheduler", args);
+
+        // atualizando valor do exec_mode pra não entrar mais no if inicial do scheduler
+        strcpy(exec_mode, "2");
+        exit(1);
+    }
+    else
+    {
+        // processo pai
+        scheduler_pid = pid;
+        printf("DEUBG: scheduler pid: %d\n", scheduler_pid);
+
+        // esperando scheduler acabar
+        wait(&status);
+
+        // abrindo em read mode
+        file_ptr = fopen("exec.txt", "r");
+
+        // lendo linhas do arquivo
+        while ((c = fgetc(file_ptr)) != EOF)
         {
-            if (fscanf(file_ptr, "un %s", program_name) == 1)
+            if (c == 'R')
             {
-                if (fscanf(file_ptr, " I=%d D=%d", &start_time, &duration_time) == 2)
+                if (fscanf(file_ptr, "un %s", program_name) == 1)
                 {
-                    printf("escalonamento robinho\n");
+                    if (fscanf(file_ptr, " I=%d D=%d", &start_time, &duration_time) == 2)
+                    {
+                        printf("escalonamento robinho\n");
 
-                    strcpy(shm_program_name, program_name);
-                    *shm_start_time = start_time;
-                    *shm_duration_time = duration_time;
+                        strcpy(shm_program_name, program_name);
+                        *shm_start_time = start_time;
+                        *shm_duration_time = duration_time;
 
-                    printf("program name, start time e duration time na memoria ja: %s %d %d\n", shm_program_name, *shm_start_time, *shm_duration_time);
+                        // printf("program name, start time e duration time na memoria ja: %s %d %d\n", shm_program_name, *shm_start_time, *shm_duration_time);
+
+                        // avisa(?) pro escalonador pegar variaveis na memoria
+                        // mandar USRSIG1 (esse avisa que é robinho)
+                    }
+                    else
+                    {
+                        printf("escalonamento real time\n");
+
+                        strcpy(shm_program_name, program_name);
+                        // printf("program name na memoria ja: %s\n", shm_program_name);
+
+                        // avisa(?) pro escalonador pegar variavel na memoria
+                        // mandar SIGUSR2 (esse avisa q é real time)
+                    }
                 }
                 else
                 {
-                    printf("escalonamento real time\n");
-
-                    strcpy(shm_program_name, program_name);
-                    printf("program name na memoria ja: %s\n", shm_program_name);
+                    printf("formatacao invalida\n");
+                    exit(1);
                 }
             }
-            else
-            {
-                printf("formatacao invalida\n");
-                exit(1);
-            }
+
+            // intervalo de 1 segundo entre processos lidos
+            // sleep(1);
         }
-        // sleep(2);
+        fclose(file_ptr);
     }
-    fclose(file_ptr);
-
-    if (shmdt(shm_program_name) == -1)
-    {
-        printf("erro no shmdt program name\n");
-        exit(3);
-    }
-
-    // liberando segmento de memoria
-    if (shmctl(segment, IPC_RMID, 0) == -1)
-    {
-        printf("erro no shmctl\n");
-        exit(4);
-    }
-
     return 0;
 }
+
+/*------------------------------------------------------------*/
+/*
+    # ideia de como pegar pid do scheduler pra enviar sinais #
+
+    Criar processo filho para chamar o scheduler através do execv() por exemplo, acho q esse tipo de exec ja basta
+    e então, o valor do fork() usado pra criar o processo filho vai passar a ser o pid do scheduler pra poder enviar
+    sinais pra ele depois.
+    Explicacao importante execv(): acho que o segundo argumento do execv() pode ser util pra evitar que o
+    scheduler não seja executado nessa primeira chamada pra pegar o pid dele
+*/
+/*------------------------------------------------------------*/
--- scheduler.c
+++ scheduler.c
@@ -5,17 +5,24 @@
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <signal.h>
 #include <string.h>
 
 #define MAX_PROGRAM_NAME_LEN 20
 #define SHM_SIZE 1024
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    if (argc > 1 && atoi(argv[1]) == 1)
+    {
+        printf("to no scheduler e o pid eh: %d\n", getpid());
+        return 0;
+    }
+
     int segment, *shm_start_time, *shm_duration_time;
     char *shm_program_name;
 
-    segment = shmget(12000, SHM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    segment = shmget(2000, SHM_SIZE, S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
     if (segment == -1)
     {
         printf("erro no shmget\n");
@@ -29,17 +36,35 @@ int main(void)
         printf("erro no shmat\n");
         exit(1);
     }
-    *shm_start_time = (int *)shmat(segment, 0, 0);
-    if (*shm_start_time == (void *)-1)
+    shm_start_time = (int *)((char *)shmat(segment, 0, 0) + (char)MAX_PROGRAM_NAME_LEN);
+    if (shm_start_time == (void *)-1)
     {
         printf("erro no shmat\n");
         exit(1);
     }
-    *shm_duration_time = (int *)shmat(segment, 0, 0);
-    if (*shm_duration_time == (void *)-1)
+    shm_duration_time = (int *)((char *)shmat(segment, 0, 0) + (char)MAX_PROGRAM_NAME_LEN + sizeof(int));
+    if (shm_duration_time == (void *)-1)
     {
         printf("erro no shmat\n");
         exit(1);
+    }
+
+    printf("codigo completo scheduler\n");
+    // printf("program name: %s\n", shm_program_name);
+    // printf("start time: %d\n", *shm_start_time);
+    // printf("duration time: %d\n", *shm_duration_time);
+    if (shmdt(shm_program_name) == -1)
+    {
+        printf("erro no shmdt() program name\n");
+        exit(1);
+    }
+    /* por que está dando erro quando eu dou detach das outras variaveis???
+    só detach de shm_program_name ta dando certo e sem memory leak */
+
+    if (shmctl(segment, IPC_RMID, 0))
+    {
+        printf("erro no shmctl() de segment\n");
+        exit(2);
     }
 
     return 0;
